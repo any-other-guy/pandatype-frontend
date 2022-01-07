5cfa9c1da6f1fb62748dcc7fc1ad0395aa2c54fe
ChunKyYoinky <ckyue@my.yorku.ca>
2022-01-07 17:06:36 -0500

    chart ok kinda..

--- src/features/TypingTest/ResultChart.js
+++ src/features/TypingTest/ResultChart.js
@@ -8,6 +8,7 @@ import {
   Title,
   Tooltip,
   Legend,
+  Filler,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
 import { perSecondWpmAction } from "./typingtestSlice";
@@ -19,11 +20,13 @@ ChartJS.register(
   LineElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  Filler
 );
 
 export const options = {
   responsive: true,
+  maintainAspectRatio: false,
   interaction: {
     mode: "index",
     intersect: false,
@@ -50,8 +53,49 @@ export const options = {
         },
         maxTicksLimit: 4,
       },
+      title: {
+        display: true,
+        text: "Words per Minute",
+        align: "center",
+        font: {
+          family: "Roboto Mono",
+        },
+      },
     },
-
+    errors: {
+      type: "linear",
+      display: true,
+      position: "right",
+      min: 0,
+      ticks: {
+        font: {
+          family: "Roboto Mono",
+        },
+        maxTicksLimit: 5, //TODO: TBD
+        stepSize: 1,
+      },
+      title: {
+        display: true,
+        text: "Errors",
+        align: "center",
+        font: {
+          family: "Roboto Mono",
+        },
+      },
+      grid: {
+        display: false,
+        drawOnChartArea: false,
+      },
+    },
+    x: {
+      display: true,
+      ticks: {
+        font: {
+          family: "Roboto Mono",
+        },
+        maxTicksLimit: 10,
+      },
+    },
   },
 };
 
@@ -69,28 +113,35 @@ const ResultChart = ({ perSecondWpm, elapsedTime }) => {
       {
         label: "wpm",
         data: stats.map((obj) => obj.wpm),
-        type: 'line',
+        type: "line",
+        fill: true,
         borderColor: "rgb(0,250,154)",
-        backgroundColor: "rgba(128,128,128, 0.5)",
+        backgroundColor: "rgba(100, 102, 105, 0.2)",
         yAxisID: "wpm",
-        tension: 0.1,
+        tension: 0.2,
       },
       {
         label: "raw",
         data: stats.map((obj) => obj.rawWpm),
-        type: 'line',
+        type: "line",
+        fill: true,
         borderColor: "rgb(100, 102, 105)",
-        backgroundColor: "rgba(128,128,128, 0.5)",
+        backgroundColor: "rgba(100, 102, 105, 0.2)",
         yAxisID: "wpm",
-        tension: 0.1,
+        tension: 0.2,
       },
-      // {
-      //   label: 'errors',
-      //   data: stats.map((obj) => obj.mistakesHere),
-      //   borderColor: 'rgb(202, 71, 84)',
-      //   backgroundColor: 'rgba(255, 255, 255, 0)',
-      //   yAxisID: 'errors',
-      // },
+      {
+        label: "errors",
+        data: stats.map((obj) => {
+          if (obj.mistakesHere === 0) obj.mistakesHere = null;
+          return obj.mistakesHere;
+        }),
+        showLine: false,
+        pointStyle: "crossRot",
+        pointBorderWidth: 2,
+        borderColor: "rgb(202, 71, 84)",
+        yAxisID: "errors",
+      },
     ],
   };
 
